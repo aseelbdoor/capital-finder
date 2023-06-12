6c0ad6bb799929ce6314e15db0959307078c83e3
aseelbdoor <aseel.bdoor2001@gmail.com>
2023-06-12 23:27:22 +0000

    finish

---
+++ READEME.md
@@ -0,0 +1,21 @@
+# Serverless Functions
+**Author :** Aseel Bdoor
+
+**Setup :**
+.env requirements : 
+- `pip install vercel`
+- `pip install requests`
+
+## How to initialize/run your application?
+```
+https://capital-finder-eight-peach.vercel.app/api/capital?
+``` 
+
+## Tests
+How do you run tests?
+- `https://capital-finder-eight-peach.vercel.app/api/capital?country=Chile`
+- `https://capital-finder-eight-peach.vercel.app/api/capital?capital=Santiago`
+- `https://capital-finder-eight-peach.vercel.app/api/capital?country=Chile&capital=Santiago`
+- `https://capital-finder-eight-peach.vercel.app/api/capital?capital=gg`
+****
+[Open the code](./api/capital.py)
--- api/capital.py
+++ api/capital.py
@@ -9,24 +9,43 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    list_of_dif=[]
-    message="testing"
     url_path = self.path
     url_components = parse.urlsplit(url_path)
     query_list = parse.parse_qsl(url_components.query)
     my_dict = dict(query_list)
+    country=my_dict.get('country')
+    capital=my_dict.get('capital')
+    r1=""
+    r2=""
+    
+    if country:
+      try:
+        url1= 'https://restcountries.com/v3.1/name/'
+        res1 = requests.get(url1+country)
+        data1 = res1.json()
+        data1=list(data1)
+        respo1=str(data1[0]["capital"][0])
+        r1=f"The capital of {country} is {respo1}"
+      except:
+        r1=f"{country} country dose not exist in our databases"
 
-    # print(111,my_dict)
-    if 'word' in my_dict:
-      word = my_dict.get('word')
-      url= 'https://api.dictionaryapi.dev/api/v2/entries/en/'
-      res = requests.get(url+word)
-      data = res.json()
-    #   print(222,data)
-    for word_data in data :
-      definition = word_data['meanings'][0]['definitions'][0]['definition']
-      message = str(definition)
-      list_of_dif.append(message)
-    print(2222,list_of_dif)
-    self.wfile.write(message.encode())
+    if capital:
+      try:
+        url2= 'https://restcountries.com/v3.1/capital/'
+        res2 = requests.get(url2+capital)
+        data2 = res2.json()
+        data2=list(data2)
+        respo2=str(data2[0]["name"]["common"])
+        r2=f"{capital} is the capital of {respo2}"
+      except:
+        r2=f"{capital} capital dose not exist in our databases"
+
+    if len(r1)>0 and len(r2)>0:
+      final=r1+"\n"+r2
+    elif len(r2)==0:
+      final=r1
+    elif len(r1)==0:
+      final=r2
+  
+    self.wfile.write(final.encode())
     return
